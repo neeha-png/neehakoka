236796938386f5999dd2fd38e2beb0cdcc732e3e
neeha-png <neehasm0@gmail.com>
2026-05-29 13:52:42 +0530

    fix
    Revert configuration back to static rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: cloudflare()
-});
+   // https://astro.build/config
+   export default defineConfig({});
