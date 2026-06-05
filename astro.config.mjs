fcb56fb4e64225490a797029821189678b8c8724
neeha-png <neehasm0@gmail.com>
2026-06-05 19:24:16 +0530

    switch to native direct wrangler cli deployment method

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', // or 'hybrid'
-  adapter: cloudflare({
-    sessionKVBindingName: "" // 👈 Changed to an empty string to bypass the strict text requirement
-  })
+  output: 'server',
+  adapter: cloudflare()
 });
