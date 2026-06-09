aa7aa7bd0c377ab72e545b7d5c399ee94ad7b7c3
neeha-png <neehasm0@gmail.com>
2026-06-09 22:06:19 +0530

    fix
    wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
+    imageService: 'compile',
     platformProxy: {
       enabled: true
     }
